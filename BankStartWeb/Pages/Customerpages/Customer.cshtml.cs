71780fa125328ab4f4f7de1c510604f07c58b678
Fredrik Swan Andreassen <91258816+WedgieJones@users.noreply.github.com>
2022-03-25 12:39:19 +0100

    Added list of accounts to Customer

--- BankStartWeb/Pages/Customerpages/Customer.cshtml.cs
+++ BankStartWeb/Pages/Customerpages/Customer.cshtml.cs
@@ -18,8 +18,8 @@ namespace BankStartWeb.Pages.Customerpages
         public string Surname { get; set; }
         public string Streetaddress { get; set; }
         public string City { get; set; }
-        public List<Account> Accounts { get; set; } = new List<Account>();
-        public int customerId { get; set; }
+        public IEnumerable<Account> Accounts { get; set; } 
+        public int CustomerId { get; set; }
 
         public class Account
 		{
@@ -31,14 +31,26 @@ namespace BankStartWeb.Pages.Customerpages
 
         public void OnGet( int customerId)
         {
-            var customer = _context.Customers.First(x => x.Id == customerId);
-            Givenname = customer.Givenname;
-            Surname = customer.Surname;
-            Streetaddress = customer.Streetaddress;
-            City = customer.City;
-            
-            Accounts = _context.Accounts.Select(Id from )
+			var customer = _context.Customers.First(x => x.Id == customerId);
+			Givenname = customer.Givenname;
+			Surname = customer.Surname;
+			Streetaddress = customer.Streetaddress;
+			City = customer.City;
+
+
+			Accounts = (from cust in _context.Customers
+                       join account in _context.Accounts
+                       on cust.Id equals account.CustomerId
+                       where cust.Id == customerId
+                       select new Account {
+                           Id = account.Id,
+                           AccountType = account.AccountType,
+                           Customerid = customerId
+					   });
+			
+            //var result = _context.Accounts.Where(x => x.CustomerId == customerId).ToList();
+             
         }
-       
-    }
+
+	}
 }
