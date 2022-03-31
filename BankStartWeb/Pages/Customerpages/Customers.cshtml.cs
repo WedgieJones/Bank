602516904a8fec700a144799aa13f129228554c1
WedgieJones <fredrikswan@yahoo.no>
2022-03-31 14:46:03 +0200

    Added search and sort

--- BankStartWeb/Pages/Customerpages/Customers.cshtml.cs
+++ BankStartWeb/Pages/Customerpages/Customers.cshtml.cs
@@ -7,6 +7,8 @@ namespace BankStartWeb.Pages.Customerpages
     public class CustomersModel : PageModel
     {
 		private readonly ApplicationDbContext _context;
+		[BindProperty (SupportsGet = true)]
+		public string SearchWord { get; set; }
 
 		public List<Customer> Customers { get; set; }
 
@@ -23,9 +25,38 @@ namespace BankStartWeb.Pages.Customerpages
 			public int Id { get; set; } 
 
 		}
-		public void OnGet()
+		public void OnGet(string col = "firstname", string order = "asc")
 		{
-			Customers = _context.Customers.Take(30).Select(s =>
+			var cust = _context.Customers.Take(30);
+
+			if(!string.IsNullOrEmpty(SearchWord))
+			cust = cust.Where(c => c.Givenname.Contains(SearchWord)
+							||  c.Surname.Contains(SearchWord));
+
+
+			if(col == "firstName") 
+			{
+				if (order == "asc")
+					cust = cust.OrderBy(cu => cu.Givenname);
+				else
+					cust = cust.OrderByDescending(cu => cu.Givenname);
+			}
+			else if(col== "surname")
+			{
+				if(order =="asc")
+					cust = cust.OrderBy(cu => cu.Surname);
+				else
+					cust = cust.OrderByDescending(cu => cu.Surname);
+			}
+			else if (col == "nationalid")
+			{
+				if (order == "asc")
+					cust = cust.OrderBy(cu => cu.NationalId);
+				else
+					cust = cust.OrderByDescending(cu => cu.NationalId);
+			}
+
+			Customers = cust.Select(s =>
 			new Customer
 			{
 				Givenname = s.Givenname,
