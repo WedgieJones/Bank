27e9006977c4099924995b845213fa08cece45b2
Fredrik Swan Andreassen <91258816+WedgieJones@users.noreply.github.com>
2022-04-04 13:09:30 +0200

    Added sort and search

--- BankStartWeb/Pages/Customerpages/Customers.cshtml.cs
+++ BankStartWeb/Pages/Customerpages/Customers.cshtml.cs
@@ -1,4 +1,5 @@
 using BankStartWeb.Data;
+using BankStartWeb.Infrastructure.Paging;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,7 +10,10 @@ namespace BankStartWeb.Pages.Customerpages
 		private readonly ApplicationDbContext _context;
 		[BindProperty (SupportsGet = true)]
 		public string SearchWord { get; set; }
-
+		public int PageNo { get; set; }
+		public int TotalPageCount { get; private set; }
+		public string SortCol { get; set; }
+		public string SortOrder { get; set; }
 		public List<Customer> Customers { get; set; }
 
 		public CustomersModel(ApplicationDbContext context)
@@ -25,38 +29,50 @@ namespace BankStartWeb.Pages.Customerpages
 			public int Id { get; set; } 
 
 		}
-		public void OnGet(string col = "firstname", string order = "asc")
+		public void OnGet(string searchWord, string col = "Givenname", string order = "asc", int pageno = 1 )
 		{
-			var cust = _context.Customers.Take(30);
+			PageNo = pageno;
+			SortCol = col;
+			SortOrder = order;
+			SearchWord = searchWord;
+
+			var cust = _context.Customers.Take(600);
 
 			if(!string.IsNullOrEmpty(SearchWord))
 			cust = cust.Where(c => c.Givenname.Contains(SearchWord)
 							||  c.Surname.Contains(SearchWord));
 
 
-			if(col == "firstName") 
-			{
-				if (order == "asc")
-					cust = cust.OrderBy(cu => cu.Givenname);
-				else
-					cust = cust.OrderByDescending(cu => cu.Givenname);
-			}
-			else if(col== "surname")
-			{
-				if(order =="asc")
-					cust = cust.OrderBy(cu => cu.Surname);
-				else
-					cust = cust.OrderByDescending(cu => cu.Surname);
-			}
-			else if (col == "nationalid")
-			{
-				if (order == "asc")
-					cust = cust.OrderBy(cu => cu.NationalId);
-				else
-					cust = cust.OrderByDescending(cu => cu.NationalId);
-			}
+			cust = cust.OrderBy(col, order == "asc" ? ExtensionMethods.QuerySortOrder.Asc :
+				ExtensionMethods.QuerySortOrder.Desc);
 
-			Customers = cust.Select(s =>
+
+			//if(col == "firstName") 
+			//{
+			//	if (order == "asc")
+			//		cust = cust.OrderBy(cu => cu.Givenname);
+			//	else
+			//		cust = cust.OrderByDescending(cu => cu.Givenname);
+			//}
+			//else if(col== "surname")
+			//{
+			//	if(order =="asc")
+			//		cust = cust.OrderBy(cu => cu.Surname);
+			//	else
+			//		cust = cust.OrderByDescending(cu => cu.Surname);
+			//}
+			//else if (col == "nationalid")
+			//{
+			//	if (order == "asc")
+			//		cust = cust.OrderBy(cu => cu.NationalId);
+			//	else
+			//		cust = cust.OrderByDescending(cu => cu.NationalId);
+			//}
+
+			var pageResult = cust.GetPaged(pageno, 20);
+			TotalPageCount = pageResult.PageCount;
+
+			Customers = pageResult.Results.Select(s =>
 			new Customer
 			{
 				Givenname = s.Givenname,
