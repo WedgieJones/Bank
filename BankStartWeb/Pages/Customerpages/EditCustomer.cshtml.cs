c9b7d073771bac5ac6b3ec9116ef8d796180122f
WedgieJones <fredrikswan@yahoo.no>
2022-05-04 11:36:19 +0200

    Minor adjustments. Cleaned sidebar

---
+++ BankStartWeb/Pages/Customerpages/EditCustomer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankStartWeb.Pages.Customerpages
+{
+    public class EditCustomerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
